71aad46a99a219d20b081fc8a5a6dd613693eb92
Allen Liao <name50095@gmail.com>
2023-08-19 23:54:23 +0800

    update AstroNvim custom config

--- astroNvim/.config/nvim/lua/user/init.lua
+++ astroNvim/.config/nvim/lua/user/init.lua
@@ -18,6 +18,27 @@ return {
           -- Configuration here, or leave empty to use defaults
         })
       end,
-    }
-  }
+    },
+
+    -- Overriding the default astronvim setting
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true, -- show the dotfile in the file explorer
+          },
+        },
+      },
+    },
+  },
+
+  options = function(local_vim) -- parameter is the default table to be overridden
+    -- nil is the same as a key not being set, so you cannot use nil reliably
+    -- when using the table override notation
+    -- local_vim.clipboard = nil -- set a value to nil to remove it from the table
+    local_vim.opt.scrolloff = 999
+    -- return modified table
+    return local_vim
+  end,
 }

458429b3cb6fd3e2d6b084cbdb806f5d135eb452
Allen Liao <namo50095@gmail.com>
2025-02-03 15:27:01 +0800

    update astronvim config

--- nvim-astronvim/lua/community.lua
+++ nvim-astronvim/lua/community.lua
@@ -7,6 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.scrolling.mini-animate" },
   { import = "astrocommunity.git.diffview-nvim" },

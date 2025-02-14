7da3bde24896dfa106cc129e19a5dcccad5fe6ae
Allen Liao <namo50095@gmail.com>
2025-02-14 20:57:33 +0800

    update lazyvim config

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LazyVim auto format
+vim.g.autoformat = true
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = true
+
+local opt = vim.opt
+
+opt.scrolloff = 999 -- Lines of context
